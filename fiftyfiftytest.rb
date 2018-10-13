5058fd1ddea7a4c633b54336f5b3e7465e4e505d
Steven <ekczile@gmail.com>
2018-10-13 17:41:28 -0400

    2nd failure

--- fiftyfiftytest.rb
+++ fiftyfiftytest.rb
@@ -9,4 +9,7 @@ class Test_50_50 < Minitest::Test
     def test_for_ticket
         assert_equal(Array, my_ticket(1, 1).class)
     end
+    def test_number_in_array
+        assert_equal(["1234"], my_ticket("1234","1234"))
+    end
 end
