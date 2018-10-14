ad4d1ded4b85a9ed809e62fc92f8e721b4407da2
Steven <ekczile@gmail.com>
2018-10-14 15:43:02 -0400

    3rd pass

--- fiftyfiftytest.rb
+++ fiftyfiftytest.rb
@@ -8,7 +8,8 @@ class Test_50_50 < Minitest::Test
 def test_for_array
     assert_equal(Array, pick_a_winner(["2123"],["2222"]).class)
 end
-def test_for_shit
+def test_for_match
     assert_equal(["1234"], pick_a_winner(["1234", "4423", "4412", "5555"], ["1234", "4444", "1235"]))
+    assert_equal(["1234", "4423"], pick_a_winner(["1234", "4423", "4412", "5555"], ["1234", "4423", "1235"]))
 end
 end
