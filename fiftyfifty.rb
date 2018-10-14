59328243b8cd6142f07c3dae10f2d943458d1d80
Steven <ekczile@gmail.com>
2018-10-14 15:37:40 -0400

    2nd success

--- fiftyfifty.rb
+++ fiftyfifty.rb
@@ -1,4 +1,10 @@
 def pick_a_winner(tickets, winning_tickets)
     array = []
+    winning_tickets.each do |v|
+        if tickets.include?(v)
+            array << v
+        end
+    end
+    array
 end
 
--- fiftyfiftytest.rb
+++ fiftyfiftytest.rb
@@ -6,6 +6,9 @@ class Test_50_50 < Minitest::Test
         assert_equal(1,1)
     end
 def test_for_array
-    assert_equal(Array, pick_a_winner(2,2).class)
+    assert_equal(Array, pick_a_winner(["2123"],["2222"]).class)
+end
+def test_for_shit
+    assert_equal(["1234"], pick_a_winner(["1234", "4423", "4412", "5555"], ["1234", "4444", "1235"]))
 end
 end
